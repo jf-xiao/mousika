2622ee28458d661afe7dd59800cab5d1f947b45f
xiaojf <294825811@qq.com>
2014-10-13 21:43:14 +0800

    代码重构

--- mousika_code_tool/src/com/mousika/tool/view/ContentView.java
+++ mousika_code_tool/src/com/mousika/tool/view/ContentView.java
@@ -9,33 +9,34 @@ import javax.swing.JTextArea;
  * @author xiaojf 294825811@qq.com
  */
 public class ContentView extends JFrame {
-    
+
     private JScrollPane scrollPane;
     private JTextArea textArea;
-    
+
     /**
      * 初始化
      */
-    public void init(){
-        this.setBounds(200,200,710,500);
+    public void init() {
+        this.setBounds(200, 200, 710, 500);
         scrollPane = new JScrollPane();
         this.setContentPane(scrollPane);
-        
+
         textArea = new JTextArea();
         scrollPane.setViewportView(textArea);
-        
+
     }
-    
+
     /**
      * 显示
+     * 
      * @param title
      * @param fileStream
      */
-    public void showView(String title,String fileStream){
+    public void showView(String title, String fileStream) {
         this.init();
         this.setTitle(title);
         textArea.setText(fileStream);
         this.setVisible(true);
     }
-    
+
 }
